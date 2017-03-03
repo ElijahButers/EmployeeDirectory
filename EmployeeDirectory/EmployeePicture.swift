2ca7420a350026cc30bf5fc4508a4ae2c9910456
ElijahButers <Elijahbuters@gmail.com>
2017-03-03 17:03:12 +0200

    Added a new cocoa touch class to the EmployeeDirectory group folder that named the class EmployeePicture and made it a subclass of NSManagedObject, where NSManagedObject was created as a subclass for the new EmployeePicture entity, this subclass will let us access the new entity from code.

---
+++ EmployeeDirectory/EmployeePicture.swift
@@ -0,0 +1,13 @@
+//
+//  EmployeePicture.swift
+//  EmployeeDirectory
+//
+//  Created by User on 3/3/17.
+//  Copyright © 2017 Razeware. All rights reserved.
+//
+
+import UIKit
+
+class EmployeePicture: NSManagedObject {
+
+}
