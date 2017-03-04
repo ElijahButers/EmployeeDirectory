2c27ae1689b7bbfa9fac96e641f2b8952abe20a6
ElijahButers <Elijahbuters@gmail.com>
2017-03-04 14:50:27 +0200

    Updated code in the configureView() in the EmployeeDetailViewController where the employee detail view will only have a small picture and therefore only needs the thumbnail version.

--- EmployeeDirectory/EmployeeDetailViewController.swift
+++ EmployeeDirectory/EmployeeDetailViewController.swift
@@ -73,7 +73,7 @@ private extension EmployeeDetailViewController {
 
     title = employee.name
 
-    let image = UIImage(data: employee.picture)
+    let image = UIImage(data: employee.pictureThumbnail)
     headShotImageView.image = image
 
     nameLabel.text = employee.name
